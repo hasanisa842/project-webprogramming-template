20b9f7de06fd3ebb7b97bfebc3a2e6c176dee529
hasanisa842 <mhisa93@gmail.com>
2023-01-04 18:49:42 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Controllers\ListingController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [ListingController::class, 'index'])
+    ->name('listings.index');
+
+Route::get('/new', [ListingController::class, 'create'])
+    ->name('listings.create');
+
+Route::post('/new', [ListingController::class, 'store'])
+    ->name('listings.store');
+
+Route::get('/dashboard', function (Request $request) {
+    return view('dashboard', [
+        'listings' => $request->user()->listings
+    ]);
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+Route::get('/{listing}', [ListingController::class, 'show'])
+    ->name('listings.show');
+
+Route::get('/{listing}/apply', [ListingController::class, 'apply'])
+    ->name('listings.apply');
